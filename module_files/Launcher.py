401075a03d9d0caaf4322da55d266b431157c971
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2022-10-19 13:28:38 +0200

    Add files via upload

--- module_files/Launcher.py
+++ module_files/Launcher.py
@@ -4,6 +4,62 @@ Created on Thu Aug 18 10:59:00 2022
 
 @author: flore
 """
+import boto3
+from collections import Counter, deque
+import csv
+import datetime as dt
+from datetime import timezone
+from dateutil.parser import parse
+from eth_account import Account
+import facebook_scraper  as fb
+from functools import partial
+from ftlangdetect import detect
+from geopy.geocoders import Nominatim
+import html
+from idlelib.tooltip import Hovertip
+from iso639 import languages
+import itertools
+import json
+import keyboard
+import libcloud
+from lxml.html.clean import Cleaner
+import numpy as np
+from operator import itemgetter
+import os
+import pandas as pd
+from pathlib import Path
+import pickle
+from PIL import Image, ImageTk, ImageFile
+from plyer import notification
+import pytz
+from queue import Queue
+import random
+import re
+import requests
+from requests_html import HTML
+from requests_html import HTMLSession
+from scipy.special import softmax, expit
+import shutils
+import snscrape.modules
+import string
+import sys
+import threading
+import time
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+import tldextract
+import transformers
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig, TFAutoModelForSequenceClassification
+import unicodedata
+import urllib.request
+import warnings
+import web3
+from web3 import Web3, HTTPProvider
+import webbrowser
+import yake
+
+
 
 import requests
 import web3
@@ -33,7 +89,7 @@ abis["ConfigRegistry"] = requests.get("https://raw.githubusercontent.com/Mathias
 contract = w3.eth.contract(contracts["ConfigRegistry"], abi=abis["ConfigRegistry"]["abi"])
 
 for value in ["_moduleHashContracts","_moduleHashSpotting","_moduleHashSpotChecking","_moduleHashApp"]:
-    print(value)
+    #print(value)
     success = False
     trials = 0
     delay = 5
@@ -41,7 +97,7 @@ for value in ["_moduleHashContracts","_moduleHashSpotting","_moduleHashSpotCheck
     while(trials < 5):
         try:
             hashValue = contract.functions.get(value).call()
-            print(hashValue)
+            #print(hashValue)
             code = downloadFile2Code(hashValue)
             success = True
             break
